4d1bc9d35d244e75c3dcba0c9f9b6d10ee906f56
layswim <118370938+layswim@users.noreply.github.com>
2024-10-26 10:31:04 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,3 @@
-/*import { useEffect, useState } from 'react';
-import './App.css';
-
-type ProdutoType = {
-  id: number;
-  nome: string;
-  preco: string;
-  descricao: string;
-  imagem: string;
-};
-
-type UserType = {
-  id: string;
-  nome: string;
-  email: string;
-  created_at: string;
-  updated_at: string;
-};
-
-function App() {
-  const [nome, setNome] = useState("");
-  const [produtos, setProdutos] = useState<ProdutoType[]>([]);
-  const [usuarios, setUsuarios] = useState<UserType[]>([]);
-
-  useEffect(() => {
-    setNome("Guilherme Terenciani");
-
-    // buscar os dados do backEnd para produtos
-    fetch("https://one022a-marketplace2.onrender.com/produtos")
-      .then(resposta => resposta.json())
-      .then(dados => setProdutos(dados));
-
-    // buscar os dados do backEnd para usuarios
-    fetch("https://one022a-marketplace2.onrender.com/usuarios")
-      .then(resposta => resposta.json())
-      .then(dados => setUsuarios(dados));
-  }, []);
-
-  return (
-    <>
-      <h1>{nome}</h1>
-      <div className="produtos-container">
-        {produtos.map(produto => (
-          <div key={produto.id} className="produto-item">
-            <h1>{produto.nome}</h1>
-            <div className='container-imagem'>
-              <img src={produto.imagem} alt="Imagem do celular" />
-            </div>
-            <p>{produto.preco}</p>
-            <p>{produto.descricao}</p>
-          </div>
-        ))}
-      </div>
-      <div className="usuarios-container">
-        {usuarios.map(usuario => (
-          <div key={usuario.id} className="usuario-item">
-            <h1>{usuario.nome}</h1>
-            <p>{usuario.email}</p>
-            <p>{usuario.created_at}</p>
-            <p>{usuario.updated_at}</p>
-          </div>
-        ))}
-      </div>
-    </>
-  );
-}
-
-export default App;
-*/
-
 import { useEffect, useState } from 'react';
 import './App.css';
 
@@ -81,7 +11,7 @@ type ProdutoType = {
 
 type UserType = {
   id: string;
-  nome: string;
+  name: string;
   email: string;
   created_at: string;
   updated_at: string;
@@ -95,13 +25,13 @@ function App() {
   useEffect(() => {
     setNome("Guilherme Terenciani");
 
-    // buscar os dados do backEnd para produtos
-    fetch("https://one022a-marketplace2.onrender.com/produtos")
+   
+    fetch("https://one022a-marketplace-001o.onrender.com/produtos")
       .then(resposta => resposta.json())
       .then(dados => setProdutos(dados));
 
-    // buscar os dados do backEnd para usuarios
-    fetch("https://one022a-marketplace2.onrender.com/usuarios")
+  
+    fetch("https://one022a-marketplace-001o.onrender.com/usuarios")
       .then(resposta => resposta.json())
       .then(dados => setUsuarios(dados));
   }, []);
@@ -111,7 +41,7 @@ function App() {
       <h1 className="app-title">Cadastro de produtos e usuários</h1>
       <div className="main-container">
         <div className="produtos-container">
-          <h2 className="section-header">Produtos</h2>
+          <h2 className="section-header">Produtos</h2> 
           {produtos.map(produto => (
             <div key={produto.id} className="produto-item">
               <h1>{produto.nome}</h1>
@@ -123,11 +53,12 @@ function App() {
             </div>
           ))}
         </div>
+        
         <div className="usuarios-container">
-          <h2 className="section-header">Usuários</h2>
+          <h2 className="section-header">Usuários</h2> 
           {usuarios.map(usuario => (
             <div key={usuario.id} className="usuario-item">
-              <h1>{usuario.nome}</h1>
+              <h1>{usuario.name}</h1>
               <p>{usuario.email}</p>
               <p>{usuario.created_at}</p>
               <p>{usuario.updated_at}</p>
